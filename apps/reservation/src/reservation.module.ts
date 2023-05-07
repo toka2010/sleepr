f5a272dc94428d0f556c2decfe639a3d7d9d0ef7
toka.fawy <toka.fawy@gmail.com>
2023-05-07 17:45:47 +0300

    build microservice day1

--- apps/reservation/src/reservation.module.ts
+++ apps/reservation/src/reservation.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ReservationService } from './reservation.service';
 import { ReservationController } from './reservation.controller';
 import { DataBaseModule } from '@app/common';
+import { ReservationRepo } from './repos/reservation.repo';
 
 @Module({
   imports:[DataBaseModule],
   controllers: [ReservationController],
-  providers: [ReservationService]
+  providers: [ReservationService , ReservationRepo]
 })
 export class ReservationModule {}
